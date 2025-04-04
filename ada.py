d56108c604dcc9d5111b36f815b25cfa2c74079c
MalenaSclippa <msclippa@mail.austral.edu.ar>
2025-04-04 12:21:05 -0300

    Update2 ada.py

--- ada.py
+++ ada.py
@@ -1,10 +1,8 @@
 def ada():
- firstname= "AdA"
- secondname= "LoVeLAce"
- fullname= f"{firstname} {secondname}" #para unir dos variables
- print(fullname.lower()) #minuscula
- print(fullname.title()) #primera letra de cada palabra mayuscula
- print(fullname.upper()) #mayusculaprint
- espacio= " "
- print(8*espacio+fullname.lower()) 
-
+	firstname= "AdA"
+	secondname= "LoVeLAce"
+	fullname= f"{firstname} {secondname}" #para unir dos variables
+	print(fullname.lower()) #minuscula
+	print(fullname.title()) #primera letra de cada palabra mayuscula
+	print(fullname.upper()) #mayusculaprint
+	print(f"\t{fullname.lower()}") 
