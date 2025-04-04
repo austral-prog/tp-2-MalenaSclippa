51e123dfda5ea7fbaaab986b0d3447964c01de78
MalenaSclippa <msclippa@mail.austral.edu.ar>
2025-04-04 12:50:27 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,13 +1,14 @@
 def change():
-    expense = 23.75
-    money = 100
-    print("ingresar gasto:")
-    print(expense)
-    print("dinero recibido")
-    print(money)
-    vuelto = money - expense
+    gasto = 23.75
+    dinero_recibido = 100
+    print("Ingresar gasto:")
+    print(gasto)
+    print("Dinero recibido")
+    print(dinero_recibido)
+    vuelto = dinero_recibido - gasto
     pesos = int(vuelto)
-    centavos = int(round((vuelto - pesos) *100))
+    centavos = int(round((vuelto - pesos) * 100))
+
     print("\nVuelto\n")
-    print(f"pesos:\n{pesos}")
-    print(f"centavos:\n{centavos}")
+    print(f"Pesos:\n{pesos}")
+    print(f"Centavos:\n{centavos}")
